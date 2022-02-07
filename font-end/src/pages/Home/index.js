4e2bb97e494fe6c169d1e67a837f69723589404c
HoaiNam071001 <nam215543694@gmail.com>
2022-02-07 12:30:20 +0700

    ư

--- font-end/src/pages/Home/index.js
+++ font-end/src/pages/Home/index.js
@@ -45,26 +45,3 @@ function App() {
   );
 }
 export default App;
-
-
-// export default class PersonList extends React.Component {
-//   state = {
-//     persons: []
-//   }
-
-//   componentDidMount() {
-//     axios.get('http://localhost:3060/')
-//       .then(res => {
-//         const persons = res.data;
-//         this.setState({ persons });
-//       })
-//   }
-
-//   render() {
-//     return (
-//       <ul>
-//         { this.state.persons.map(person => <li key={person.name}>{person.name}</li>)}
-//       </ul>
-//     )
-//   }
-// }
